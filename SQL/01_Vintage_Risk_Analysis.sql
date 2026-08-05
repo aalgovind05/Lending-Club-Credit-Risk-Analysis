5dd7c4580d2dd48a3847bc417ff9746e9d8ec6b2
aal_govind <aalgovind05@gmail.com>
2026-08-05 13:45:09 +0530

    info schema columns

--- SQL/01_Vintage_Risk_Analysis.sql
+++ SQL/01_Vintage_Risk_Analysis.sql
@@ -29,11 +29,11 @@ FROM status_rates
 SELECT TOP 1
     issue_year,
     grade,
-    (charged_off_rate - avg_charged_off_rate) changed_off_rate_diff,
+    (charged_off_rate - avg_charged_off_rate) charged_off_rate_diff,
     charged_off_rate AS highest_charged_off_rate,
     avg_charged_off_rate
 FROM avg_charged_off
 WHERE charged_off_rate > avg_charged_off_rate
-ORDER BY changed_off_rate_diff DESC
+ORDER BY charged_off_rate_diff DESC
 
 
