d8186fb406ded4f4f45710abb7b8ff28a058f6d0
aal_govind <aalgovind05@gmail.com>
2026-08-05 13:09:23 +0530

    charged off rate Risk

--- SQL/01_Vintage_Risk_Analysis.sql
+++ SQL/01_Vintage_Risk_Analysis.sql
@@ -6,6 +6,8 @@ defined as the year where Grade A's charged-off rate most exceeds the all-year G
 Use a CTE to compute the baseline Grade A average, 
 then join back to the year-level rates to find the outlier.*/
 
+
+WITH status_rates AS(
 SELECT 
     YEAR(issue_date) AS issue_year,
     grade,
@@ -13,3 +15,20 @@ SELECT
 FROM happen
 WHERE grade = 'A'
 GROUP BY YEAR(issue_date), grade
+),
+avg_charged_off_rate AS(
+SELECT 
+    issue_year,
+    charged_off_rate,
+    (SELECT AVG(charged_off_rate) FROM status_rates) AS avg_charged_off_rate
+FROM status_rates
+ORDER BY issue_year
+)
+SELECT 
+    issue_year,
+    charged_off_rate,
+    avg_charged_off_rate,
+    charged_off_rate - avg_charged_off_rate AS deviation_from_avg
+FROM avg_charged_off_rate
+ORDER BY deviation_from_avg DESC
+
