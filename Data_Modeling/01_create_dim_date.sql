744dc7f64f574f6d78b17186c3581d8ba0bfeb06
Govind Aal <aalgovind05@gmail.com>
2026-08-15 14:46:02 +0530

    created a dim_grade

---
+++ Data_Modeling/01_create_dim_date.sql
@@ -0,0 +1,13 @@
+CREATE TABLE dim_date (
+        grade_key INT IDENTITY(1,1) PRIMARY KEY,
+        grade VARCHAR(5) NOT NULL,
+        sub_grade VARCHAR(5) NOT NULL);
+
+
+INSERT INTO dim_date(grade, sub_grade)
+SELECT DISTINCT grade, 
+    sub_grade
+FROM happen
+ORDER BY grade, sub_grade;
+
+SELECT * FROM dim_date
