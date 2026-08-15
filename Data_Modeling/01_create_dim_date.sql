a6b4a32a5d7a871d601153c7d964e7017fce7771
Govind Aal <aalgovind05@gmail.com>
2026-08-15 14:49:48 +0530

    created a dim_loan_status Table for stat schema

--- Data_Modeling/01_create_dim_date.sql
+++ Data_Modeling/01_create_dim_date.sql
@@ -1,13 +1,13 @@
-CREATE TABLE dim_date (
+CREATE TABLE dim_grade (
         grade_key INT IDENTITY(1,1) PRIMARY KEY,
         grade VARCHAR(5) NOT NULL,
         sub_grade VARCHAR(5) NOT NULL);
 
 
-INSERT INTO dim_date(grade, sub_grade)
+INSERT INTO dim_grade(grade, sub_grade)
 SELECT DISTINCT grade, 
     sub_grade
 FROM happen
 ORDER BY grade, sub_grade;
 
-SELECT * FROM dim_date
+SELECT * FROM dim_grade;
