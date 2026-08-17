d7624b8df4c828d8269a703e6a63ff44285cc8b4
Govind Aal <aalgovind05@gmail.com>
2026-08-17 14:28:00 +0530

    borrower and state keys in happen table

--- SQL_Data_Modeling/04_create_dim_geography.sql
+++ SQL_Data_Modeling/04_create_dim_geography.sql
@@ -10,3 +10,24 @@ ORDER BY borrower_state;
 
 
 SELECT * FROM dim_geography ;
+
+-- Insert state_key into happen table
+
+ALTER TABLE happen ADD state_key    INT;
+
+UPDATE f
+SET    f.state_key = d.state_key
+FROM   happen f
+JOIN   dim_geography d
+    ON UPPER(f.borrower_state) = UPPER(d.borrower_state);
+
+
+-- Check state_key in happen table
+
+SELECT TOP(10) * FROM happen;
+
+-- Verify
+SELECT
+    COUNT(*)                                              AS total_rows,
+    SUM(CASE WHEN state_key IS NULL THEN 1 ELSE 0 END)   AS null_state_key
+FROM happen;
--- SQL_Data_Modeling/05_create_dim_borrower.sql
+++ SQL_Data_Modeling/05_create_dim_borrower.sql
@@ -6,8 +6,40 @@ CREATE TABLE dim_borrower (
 );
 
 INSERT INTO dim_borrower (employment_length, home_ownership, verification_status)
-SELECT DISTINCT employment_length, home_ownership, verification_status
+SELECT 
+    DISTINCT employment_length, 
+    home_ownership, 
+    verification_status
 FROM happen
 ORDER BY employment_length, home_ownership, verification_status;
 
+
 SELECT * FROM dim_borrower;
+
+-- Add borrower_key into happen (main) table
+
+ALTER TABLE happen ADD borrower_key INT;
+
+
+
+UPDATE f
+SET    f.borrower_key = d.borrower_key
+FROM   happen f
+JOIN   dim_borrower d
+    ON  ISNULL(f.employment_length, 'Unknown') 
+      = ISNULL(d.employment_length, 'Unknown')
+    AND ISNULL(f.home_ownership, 'Unknown') 
+      = ISNULL(d.home_ownership, 'Unknown')
+    AND ISNULL(f.verification_status, 'Unknown') 
+      = ISNULL(d.verification_status, 'Unknown');
+
+
+-- Check happen table for borrower_key presence
+
+SELECT TOP(10)* FROM happen
+
+-- Verify
+SELECT
+    COUNT(*)                                                AS total_rows,
+    SUM(CASE WHEN borrower_key IS NULL THEN 1 ELSE 0 END)  AS null_borrower_key
+FROM happen;
