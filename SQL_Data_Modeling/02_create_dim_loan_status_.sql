e9d6192b5ea2fc10566c72a65256dce6ecec5a56
Govind Aal <aalgovind05@gmail.com>
2026-08-17 13:12:46 +0530

    added purpose dim keys

--- SQL_Data_Modeling/02_create_dim_loan_status_.sql
+++ SQL_Data_Modeling/02_create_dim_loan_status_.sql
@@ -18,3 +18,27 @@ FROM happen
 ORDER BY loan_status;
 
 SELECT * FROM dim_loan_status;
+
+
+-- Add status key in main big table
+
+ALTER TABLE happen ADD status_key INT;
+
+
+UPDATE f
+SET f.status_key = d.status_key
+FROM happen f
+JOIN dim_loan_status d
+    ON f.loan_status = d.loan_status;
+
+
+--Check status key in main table 
+
+SELECT top(10)* FROM happen
+
+
+-- Verify
+SELECT
+    COUNT(*) AS total_rows,
+    SUM(CASE WHEN status_key IS NULL THEN 1 ELSE 0 END) AS null_status_key
+FROM happen;
--- SQL_Data_Modeling/03_create_dim_purpose.sql
+++ SQL_Data_Modeling/03_create_dim_purpose.sql
@@ -9,3 +9,26 @@ FROM happen
 ORDER BY purpose;
 
 SELECT * FROM dim_purpose
+
+--adding purpose_key in happen (main) table
+
+ALTER TABLE happen ADD purpose_key  INT;
+
+UPDATE f
+SET f.purpose_key = d.purpose_key
+FROM happen f
+JOIN dim_purpose d
+    ON TRIM(f.purpose) = TRIM(d.purpose);
+
+
+-- Check purpose key in happen table 
+
+SELECT TOP(10)* FROM happen
+
+
+-- Verify
+
+SELECT
+    COUNT(*) AS total_rows,
+    SUM(CASE WHEN purpose_key IS NULL THEN 1 ELSE 0 END) AS null_purpose_key
+FROM happen;
